df8c50acdfc3e32e8a0c0fa4cebdd4ef6f73904c
Gustavo Isturiz <isturizgustavo@gmail.com>
2020-05-28 16:57:57 -0400

    added link to github repo

--- src/components/About.js
+++ src/components/About.js
@@ -5,6 +5,7 @@ export default function About() {
         <div>
             <h3>About this Algorithm</h3>
             <p>The universe of the Game of Life is an infinite, two-dimensional orthogonal grid of square cells, each of which is in one of two possible states, live or dead, (or populated and unpopulated, respectively). Every cell interacts with its eight neighbours, which are the cells that are horizontally, vertically, or diagonally adjacent.</p>
+            <a href="https://github.com/gisturiz/conway_game_life" target="_blank" rel="noopener noreferrer">Check the code here</a>
         </div>
     )
 }
