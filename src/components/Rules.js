01648c71b9692bcfd07d3f6ddc215d36438bcad2
Gustavo Isturiz <isturizgustavo@gmail.com>
2020-07-13 20:28:29 -0400

    relay work

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -6,10 +6,10 @@ export default function Rules() {
             <div className="rules-section">
                 <h3>Rules</h3>
                 <ol>
-                    <li>Any live cell with fewer than two live neighbours dies, as if by underpopulation.</li>
-                    <li>Any live cell with two or three live neighbours lives on to the next generation.</li>
-                    <li>Any live cell with more than three live neighbours dies, as if by overpopulation.</li>
-                    <li>Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.</li>
+                    <li>Any live cell with fewer than two live neighbors dies, as if by under-population.</li>
+                    <li>Any live cell with two or three live neighbors lives on to the next generation.</li>
+                    <li>Any live cell with more than three live neighbors dies, as if by overpopulation.</li>
+                    <li>Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.</li>
                 </ol>
             </div>
         </div>
